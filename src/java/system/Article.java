0c2f3baedd5d077c77fd46cb618a16201e7ff728
yoannk <yoann.kraria@gmail.com>
2015-02-08 23:10:12 +0100

    modifcation Article

--- src/java/system/Article.java
+++ src/java/system/Article.java
@@ -16,9 +16,24 @@ public class Article {
     
     List<Comment> comments;
     Status status;
+    User author;
     
     String title, keywords, published_on, content, position_name;
     Long position_latitude, position_longitude;
     Image photo;
     
+    public Article(String title, String keywords, String published_on, String content, String position_name, Long position_latitude, Long position_longitude, Image photo, Status status, User author){
+        
+        this.title = title;
+        this.keywords = keywords;
+        this.published_on = published_on;
+        this.content = content;
+        this.position_name = position_name;
+        this.position_latitude = position_latitude;
+        this.position_longitude = position_longitude;
+        this.photo = photo;
+        this.status = status;
+        this.author = author;
+        
+    }
 }
